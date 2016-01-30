85de1b4e0c7d1064d968a58aa9b7dac5f3afc986
John Kitchin <jkitchin@andrew.cmu.edu>
2016-01-30 17:58:56 -0500

    init

---
+++ kitchingroup-46.el
@@ -0,0 +1,45 @@
+;;; kitchingroup-46.el --- preprint                  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2016  John Kitchin
+
+;; Author: John Kitchin <jkitchin@andrew.cmu.edu>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary: Preprint for
+;; @article{demeter-2014-elect-oxygen,
+;;   author =	 {Ethan L. Demeter and Shayna L. Hilburg and Newell R. Washburn
+;;                   and Terrence J. Collins and John R. Kitchin},
+;;   title =	 {Electrocatalytic Oxygen Evolution With an Immobilized {TAML}
+;;                   Activator},
+;;   journal =	 {J. Am. Chem. Soc.},
+;;   volume =	 136,
+;;   number =	 15,
+;;   pages =	 {5603-5606},
+;;   year =	 2014,
+;;   doi =		 {10.1021/ja5015986},
+;;   url =		 {http://dx.doi.org/10.1021/ja5015986},
+;; }
+
+;;
+
+;;; Code:
+
+(require 'cappa)
+
+(cappa-register 'kitchingroup-46)
+
+(provide 'kitchingroup-46)
+;;; kitchingroup-46.el ends here
